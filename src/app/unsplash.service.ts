027c22e39a8bc8e9d0c9272437c2af088048d255
JoshSevy <joshuasevy@outlook.com>
2021-01-19 15:49:26 -0700

    Connect to unsplash api

--- src/app/unsplash.service.ts
+++ src/app/unsplash.service.ts
@@ -1,11 +1,21 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@andular/common/http';
 import { HttpClient } from '@angular/common/http';
+
+import { environment } from '../environments/environment'
 
 @Injectable({
   providedIn: 'root'
 })
 export class UnsplashService {
-
   constructor(private http: HttpClient) { }
+
+
+  getPhoto() {
+    return this.http.get('https://api.unsplash.com/photos/random', {
+      headers: {
+        Authorization: `Client-ID ${environment.API}`,
+      }
+    })
+  }
+
 }
